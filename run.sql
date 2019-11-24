f6e5ede63bc47ea3af744cc5f04000a2d8afcd5c
Daníel Þór Þórisson <danielthorr@gmail.com>
2019-11-24 14:52:25 +0000

    Finished verkefni 3

--- run.sql
+++ run.sql
@@ -1,27 +1,95 @@
 delimiter $$
-drop procedure if exists cursorTest$$
+drop procedure if exists SingleStudentJSon$$
 
-create procedure cursorTest(inout cNumList varchar(255))
+create procedure SingleStudentJSon(in studID int, inout jsonString varchar(5000))
 begin
-    declare finished integer default 0;
-    declare cNumber char(6) default "";
+    /* Going over verkefni 1 I realized I was doing this in an inefficient way
+       and I didn't realize how I could utilize the cursor better. 
+       This time I changed some things so it is much more efficient. */
 
-    declare tmpCurs cursor for select courseNumber from Courses;
+    /* Finished is our way of ending a loop when the cursor has finished 
+       I'm using different numbers to represent completion for different cursors.*/
+    declare finished tinyint default 0;
+    declare firstLoop tinyint default 0;
+
+    declare storeFirstN varchar(50) default "";
+    declare storeLastN varchar(50) default "";
+    declare storeDob varchar(50) default "";
+    declare storeCourseN char(10) default "";
+    declare storeCourseC tinyint default 0;
+    declare storePass varchar(5) default "";
+
+    /* ---- Cursor with select ---- */
+    declare studCurs cursor for 
+        select
+            s.studentID, s.firstName, s.lastName, s.dob,
+            c.courseNumber, c.courseCredits,
+            (
+                case
+                    when r.passed = 1 then "pass"
+                    when r.passed = 0 then "fail"
+                end
+            )
+        from Students s
+        inner join Registration r on s.studentID = r.studentID
+        inner join Courses c on r.courseNumber = c.courseNumber
+        where s.studentID = 1;
+
+    /* ---- Cursor handler ---- */
     declare continue handler
         for not found set finished = 1;
-    open tmpCurs;
+    
+    /* ---- Open the cursor and loop through the data ---- */
+    open studCurs;
+    loopStudents: loop
+        fetch studCurs into studID, storeFirstN, storeLastN, storeDob, storeCourseN, storeCourseC, storePass;
 
-    getCourses: loop
-        fetch tmpCurs into cNumber;
-        if finished = 1 then
-            leave getCourses;
+        if firstLoop = 0 then 
+            set jsonString = concat(
+                jsonString,
+                '"first_name": ',
+                concat('"', storeFirstN, '", '),
+                '"last_name": ',
+                concat('"', storeLastN, '", '),
+                '"date_of_birth": ',
+                concat('"', storeDob, '", '),
+                '"courses": ['
+            );
+            set firstLoop = 1;
         end if;
-        set cNumList = concat(cNumber, ";", cNumList);
-    end loop getCourses;
+        
+        set jsonString = concat(
+            jsonString,
+            '{',
+            '"course_number": ',
+            concat('"', storeCourseN, '", '),
+            '"course_credits": ',
+            concat('"', storeCourseC, '", '),
+            '"status": ',
+            concat('"', storePass, '"'),
+            '}'
+        );
+
+        if finished = 1
+            then leave loopStudents;
+        end if;
+
+        /* We need to append a comma at the end of each row UNLESS it's the last row */
+        set jsonString = concat(jsonString, ",");
+
+    end loop loopStudents;
+
+    /* Finish the "courses: []" property with a closing bracket */
+    set jsonString = concat(jsonString, "]");
+    
+    close studCurs;
+
+    /* Prepend and append opening and closing brackets */
+    set jsonString = concat("[ {", jsonString, "} ]");
 end$$
 
-set @cNumList = ""$$
-call cursorTest(@cNumList)$$
-select @cNumList$$
+set @jsonString = ""$$
+call SingleStudentJSon(1, @jsonString)$$
+select @jsonString$$
 
 delimiter ;
--- verkefni3/Skilverkefni_3.sql
+++ verkefni3/Skilverkefni_3.sql
@@ -16,9 +16,62 @@
 		  {"first_name": "Jasmín Rós", "last_name": "Stefánsdóttir", "date_of_birth": "1996-02-29"}
 	]
 */
+delimiter $$
+drop procedure if exists StudentListJSon$$
 
+create procedure StudentListJSon(inout jsonString varchar(1000))
+begin
+    /* Finished is our way of ending a loop when the cursor has finished */
+    declare finished integer default 0;
+    declare storeFetch varchar(200) default "";
 
-/*
+    /* Just to cut down on the total amount of work that I need to do
+    to get the data into a json format, I try to concatenate everything together
+    in this select statement */
+    declare studCurs cursor for 
+        select concat(
+            '"first_name": ',
+            '"',firstName,'", ',
+            '"last_name": ', 
+            '"',lastName,'", ',
+            '"date_of_birth": ', 
+            '"',dob,'"'
+            )
+            from Students;
+    declare continue handler
+        for not found set finished = 1;
+    
+    open studCurs;
+
+    /* Start the json string off with an opening bracket [ */
+    set jsonString = "[ ";
+    loopRows: loop
+        fetch studCurs into storeFetch;
+        /* Add the fetched data to the jsonString variable and add some curly braces */
+        set jsonString = concat(jsonString, "{", storeFetch, "}");
+        if finished = 1
+            then leave loopRows;
+        end if;
+
+        /* We need to append a comma at the end of each row UNLESS it's the last row */
+        set jsonString = concat(jsonString, ",");
+    end loop loopRows;
+
+    close studCurs;
+
+    /* End the procedure by add a closing bracket to the string */
+    set jsonString = concat(jsonString, " ]");
+end$$
+
+set @jsonString = ""$$
+call StudentListJSon(@jsonString)$$
+select @jsonString$$
+
+delimiter ;
+
+/* ################################################################################
+	 --------------------------------------------------------------------------------
+	 ################################################################################
 	2:
 	Skrifið nú SingleStudentJSon()þannig að nemandinn innihaldi nú lista af þeim áföngum sem hann hefur tekið.
 	Śé nemandinn enn við nám þá koma þeir áfangar líka með.
@@ -44,8 +97,106 @@
 		]
 	}
 */
+delimiter $$
+drop procedure if exists SingleStudentJSon$$
 
-/*
+create procedure SingleStudentJSon(in studID int, inout jsonString varchar(5000))
+begin
+    /* Going over verkefni 1 I realized I was doing this in an inefficient way
+       and I didn't realize how I could utilize the cursor better. 
+       This time I changed some things so it is much more efficient. */
+
+    /* Finished is our way of ending a loop when the cursor has finished 
+       I'm using different numbers to represent completion for different cursors.*/
+    declare finished tinyint default 0;
+    declare firstLoop tinyint default 0;
+
+    declare storeFirstN varchar(50) default "";
+    declare storeLastN varchar(50) default "";
+    declare storeDob varchar(50) default "";
+    declare storeCourseN char(10) default "";
+    declare storeCourseC tinyint default 0;
+    declare storePass varchar(5) default "";
+
+    /* ---- Cursor with select ---- */
+    declare studCurs cursor for 
+        select
+            s.studentID, s.firstName, s.lastName, s.dob,
+            c.courseNumber, c.courseCredits,
+            (
+                case
+                    when r.passed = 1 then "pass"
+                    when r.passed = 0 then "fail"
+                end
+            )
+        from Students s
+        inner join Registration r on s.studentID = r.studentID
+        inner join Courses c on r.courseNumber = c.courseNumber
+        where s.studentID = 1;
+
+    /* ---- Cursor handler ---- */
+    declare continue handler
+        for not found set finished = 1;
+    
+    /* ---- Open the cursor and loop through the data ---- */
+    open studCurs;
+    loopStudents: loop
+        fetch studCurs into studID, storeFirstN, storeLastN, storeDob, storeCourseN, storeCourseC, storePass;
+
+        if firstLoop = 0 then 
+            set jsonString = concat(
+                jsonString,
+                '"first_name": ',
+                concat('"', storeFirstN, '", '),
+                '"last_name": ',
+                concat('"', storeLastN, '", '),
+                '"date_of_birth": ',
+                concat('"', storeDob, '", '),
+                '"courses": ['
+            );
+            set firstLoop = 1;
+        end if;
+        
+        set jsonString = concat(
+            jsonString,
+            '{',
+            '"course_number": ',
+            concat('"', storeCourseN, '", '),
+            '"course_credits": ',
+            concat('"', storeCourseC, '", '),
+            '"status": ',
+            concat('"', storePass, '"'),
+            '}'
+        );
+
+        if finished = 1
+            then leave loopStudents;
+        end if;
+
+        /* We need to append a comma at the end of each row UNLESS it's the last row */
+        set jsonString = concat(jsonString, ",");
+
+    end loop loopStudents;
+
+    /* Finish the "courses: []" property with a closing bracket */
+    set jsonString = concat(jsonString, "]");
+    
+    close studCurs;
+
+    /* Prepend and append opening and closing brackets */
+    set jsonString = concat("[ {", jsonString, "} ]");
+end$$
+
+set @jsonString = ""$$
+call SingleStudentJSon(1, @jsonString)$$
+select @jsonString$$
+
+delimiter ;
+
+
+/* ################################################################################
+	 --------------------------------------------------------------------------------
+	 ################################################################################
 	3:
 	Skrifið stored procedure: SemesterInfoJSon() sem birtir uplýsingar um ákveðið semester.
 	Semestrið inniheldur lista af nemendum sem eru /hafa verið á þessu semestri.
@@ -63,3 +214,69 @@
 
 -- ACHTUNG:  2 og 3 nota líka cursor!
 
+delimiter $$
+
+drop procedure if exists SemesterInfoJson $$
+
+create procedure SemesterInfoJson(in semID int, inout jsonString varchar(1000))
+begin
+
+    /* Finished is our way of ending a loop when the cursor has finished 
+    I'm using different numbers to represent completion for different cursors.*/
+    declare finished tinyint default 0;
+
+    declare storeSID int default null;
+    declare storeFirst varchar(20) default "";
+    declare storeLast varchar(20) default "";
+    declare storeCourseCount int default 0;
+
+    declare concatString varchar(200) default "";
+
+    /* ---- Cursor with select ---- */
+    declare semCursor cursor for
+        select
+            s.studentID, s.firstName, s.lastName, count(r.studentID) 
+        from Registration r 
+        inner join Students s on s.studentID = r.studentID 
+        group by r.studentID;
+
+    /* ---- Cursor handler ---- */
+    declare continue handler
+        for not found set finished = 1;
+
+    /* ---- Open the cursor and loop through the data ---- */
+    open semCursor;
+    loopStudents: loop
+        fetch semCursor into storeSID, storeFirst, storeLast, storeCourseCount;
+        set jsonString = concat(
+            '{',
+            '"student_id": ',
+            concat('"', storeSID, '", '),
+            '"first_name": ',
+            concat('"', storeFirst, '", '),
+            '"last_name": ',
+            concat('"', storeLast, '", '),
+            '"courses_taken": ',
+            concat('"', storeCourseCount, '"'),
+            '}'
+        );
+
+        if finished = 1 
+            then leave loopStudents;
+        end if;
+        
+        /* We need to append a comma at the end of each row UNLESS it's the last row */
+        set jsonString = concat(jsonString, ",");
+    end loop loopStudents;
+
+    close semCursor;
+
+    /* Prepend and append opening and closing brackets */
+    set jsonString = concat("[ ", jsonString, " ]");
+end;
+
+set @jsonString = ""$$
+call SemesterInfoJSon(3, @jsonString)$$
+select @jsonString$$
+
+delimiter ;
